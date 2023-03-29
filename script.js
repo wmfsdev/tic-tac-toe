a4fd2716c4f8378a434f31b8c05d17af3480eb91
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2023-03-30 00:24:20 +0100

    code review

--- script.js
+++ script.js
@@ -29,10 +29,9 @@ const Gameboard = (() => {    // MODULE
           this.gameState()
         }
         else if (gameboard[0][eTarget] !== '.') {
-          gameboard = gameboard[0][eTarget] // = gameboard[0][eTarget]
+          gameboard = gameboard[0][eTarget]
         }
         console.log(gameboard)
-        
       },
 
       gameState() {
@@ -57,10 +56,7 @@ const Gameboard = (() => {    // MODULE
             // ^^ Vertical wins from first to last column
             gameboard[0][0] === 'X'  && gameboard[0][4] === 'X' && gameboard[0][8] === 'X' || 
             gameboard[0][6] === 'X'  && gameboard[0][4] === 'X'  && gameboard[0][2] === 'X'
-          )
-        {
-          console.log("-----WINNER!!-----")
-        }
+          ) { console.log("---- WINNER! -----") }
       },
 
       log() {console.dir(gameboard)},
@@ -89,8 +85,7 @@ const Players = (name, playerId) => {
 
 const gameFlow = () => {
 
-  // RENDER
-  const printBoard = () => {Gameboard.renderBoard()}
+ // const printBoard = () => {Gameboard.renderBoard()}
       // PLAYER SELECT - we can come back to this. for now player names are default
       //  const nameInput = () => {
       //   const form = document.querySelector('.player-info')
@@ -98,46 +93,41 @@ const gameFlow = () => {
       //     e.preventDefault()
       //   })} - something like this?
   
-  // PLAYER MARK TILE
-  printBoard() // this possibly needs to be somewhere else entirely - it resets the entire board.
+//  printBoard() // this possibly needs to be somewhere else entirely - it resets the entire board.
               // alternatively, add an if condition so it doesn't reset until win or if array not empty etc.
 
+           
+  let turn = 0
+
+  const turnListen = () => {
+    const listen = document.querySelectorAll('button')
+      listen.forEach((button) => {
+        button.addEventListener('click', (e) => {
+          if (turn === 0) {
+            playerOne.tileId(e.target.dataset.index)
+            turn += 1
+            console.log("player one")
+          } else {
+            playerTwo.tileId(e.target.dataset.index)
+            console.log("player two")
+            turn -= 1
+          }
+        })
+  })
+  }
+
+  // const winner = () => {
+  //   // ????
+  // }
+  
 const playerOne = Players("playerOne", "0")
 const playerTwo = Players("playerTwo", "X")
 
-        
-let turn = 0
-
-const turnListen = () => {
-  const listen = document.querySelectorAll('button')
-    listen.forEach((button) => {
-      button.addEventListener('click', (e) => {
-        if (turn === 0) {
-          playerOne.tileId(e.target.dataset.index)
-          // playerOne.tileArray()
-          turn += 1
-         console.log("player one")
-       } else {
-         playerTwo.tileId(e.target.dataset.index)
-        // playerTwo.tileArray()
-         console.log("player two")
-           turn -= 1
-         }
-      })
-})
-}
-
-// const winner = () => {
-  // ??
-// }
-
-
-
+Gameboard.renderBoard()
+Gameboard.assignDataset()
 turnListen()
-  
-}
 
+}
 
 gameFlow()
-Gameboard.log()
-Gameboard.assignDataset()
+// Gameboard.log()
