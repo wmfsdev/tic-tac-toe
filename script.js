2a0f9669d1202640c1e2185aaedd3855ad437575
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2023-03-29 02:37:19 +0100

    0s and Xs alternate on button click to represent player turns. Corresponding symbols are displayed on the UI and added to the gameboard array

--- script.js
+++ script.js
@@ -3,58 +3,61 @@
 //   e.preventDefault()
 // })
 
+
 const Gameboard = (() => {    // MODULE
-    const gameboard = [
-        ['.', '.', '.', 
-         '.', '.', '.', 
-         '.', '.', '.']
-      ];
+  const gameboard = [
+      ['.', '.', '.', 
+       '.', '.', '.', 
+       '.', '.', '.']
+    ];
 
-      return {    // setup a condition to prevent multiple playerId entries OR
-                  //  figure out how it factors into player turn
-         
-        markArray(playerId) {        // takes player mark from Player object
-          const listen = document.querySelectorAll('button')
-          listen.forEach((button) => {
-            button.addEventListener('click', (e) => {
-              button.textContent = playerId
-              this.markBoard(e.target.dataset.index, playerId)
-            })
-          })  
-        },
+    return {
+      markBoard(playerId, eTarget) {        // takes player mark from Player object
+        const listen = document.querySelector(`[data-index="${eTarget}"]`)
+        listen.textContent = playerId
+        this.markArray(playerId, eTarget)
+       // console.log(playerId,eTarget)
+      },
 
-        markBoard(eTarget, playerId) {
-          console.log(eTarget)
+      markArray(playerId, eTarget) {
+       console.log(playerId, eTarget)
+        
+        if (gameboard[0][eTarget] === '.' ) {   
+           gameboard[0].splice(eTarget, 1, playerId)
+         }
+        else if (gameboard[0][eTarget] !== '.') {
+          gameboard[0] = gameboard[0]
+        } 
+   
+          console.log(gameboard)
+      },
 
-          if (gameboard[0][eTarget] === '.' ) {   
-            gameboard[0].splice(eTarget, 1, playerId)
-          }
-          else if (gameboard[0] !== '.') {
-            gameboard[0][0] = gameboard[0][0]
-          } 
-            console.log(gameboard)
-        },
+      log() {console.dir(gameboard)},
+      renderBoard() {
+        const board = document.querySelectorAll('.tiles')
+        board.forEach((tile) => {
+        const button = document.createElement('button')
+        tile.appendChild(button)})},
 
-        log() {console.dir(gameboard)},
-        renderBoard() {
-          const board = document.querySelectorAll('.tiles')
-          board.forEach((tile) => {
-          const button = document.createElement('button')
-          tile.appendChild(button)})},
-
-        assignDataset() {
-          const buttons = document.querySelectorAll('button')
-          buttons.forEach((element, index) => {
-          element.dataset.index = index   
-          });
-        },
-      }
+      assignDataset() {
+        const buttons = document.querySelectorAll('button')
+        buttons.forEach((element, index) => {
+        element.dataset.index = index   
+        });
+      },
+    }
 })()
 
 
-const Players = (name, playerId) => {     // FACTORY FUNCTION
-     
-    const tileId = () => {Gameboard.markArray(playerId)}  // Gameboard.markArray(playerId)
+const Players = (name, playerId) => { 
+    const tileId = (etarget) => {
+      Gameboard.markBoard(playerId, etarget)  // e.target.dataset.index
+    }
+
+    return { name, playerId, tileId } 
+    // const tileArray = (eTarget) => {
+    //   Gameboard.markBoard(eTarget)
+    // }
 
     // const placeMark = () => {
     //   const listen = document.querySelectorAll('button')
@@ -64,12 +67,11 @@ const Players = (name, playerId) => {     // FACTORY FUNCTION
     //     //  Gameboard.markBoard(e.target.dataset.index) // Gameboard.markBoard(e.target.dataset.index)
     //     })
     //   })
-    // }
-    return { name, playerId, tileId} 
-}
-    
+    // }   
+}   
 
 const gameFlow = () => {
+
   // RENDER
   const printBoard = () => {Gameboard.renderBoard()}
       // PLAYER SELECT - we can come back to this. for now player names are default
@@ -77,25 +79,42 @@ const gameFlow = () => {
       //   const form = document.querySelector('.player-info')
       //   form.addEventListener('submit', (e) => {
       //     e.preventDefault()
-      //   })}
-
-  // PLAYER TURN
-
-
-  // PLAYER MARK TILE
-
-  printBoard()
-
-   const playerOne = Players("playerOne", "X")
-  // playerOne.placeMark()
-   playerOne.tileId()
+      //   })} - something like this?
   
- // buttonListen()
+  // PLAYER MARK TILE
+  printBoard() // this possibly needs to be somewhere else entirely - it resets the entire board.
+              // alternatively, add an if condition so it doesn't reset until win or if array not empty etc.
+
+const playerOne = Players("playerOne", "0")
+const playerTwo = Players("playerTwo", "X")
+
+        
+let turn = 0
+
+const turnListen = () => {
+  const listen = document.querySelectorAll('button')
+    listen.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        if (turn === 0) {
+          playerOne.tileId(e.target.dataset.index)
+          // playerOne.tileArray()
+          turn += 1
+         console.log("player one")
+       } else {
+         playerTwo.tileId(e.target.dataset.index)
+        // playerTwo.tileArray()
+         console.log("player two")
+           turn -= 1
+         }
+      })
+})
+}
+
+turnListen()
+  
 }
 
 
 gameFlow()
 Gameboard.log()
 Gameboard.assignDataset()
-
-
